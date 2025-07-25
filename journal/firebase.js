083dde7c541e700783feef347aeb97725123b19b
rezkaix <reza.shadmaan@gmail.com>
2025-07-25 16:40:42 +0400

    Update firebase.js

--- journal/firebase.js
+++ journal/firebase.js
@@ -1,17 +1,1 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyDTObfs5bSzpt1fs--jGfH6oStOZZyHQNU",
-  authDomain: "rezkaix-app-82c80.firebaseapp.com",
-  projectId: "rezkaix-app-82c80",
-  storageBucket: "rezkaix-app-82c80.firebasestorage.app",
-  messagingSenderId: "664659784723",
-  appId: "1:664659784723:web:20a0d62e3a93984190fbe6"
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
